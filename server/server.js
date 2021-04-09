2da2bfe9a4d8e0d0584556d384b8400d2c631980
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2021-04-09 16:50:03 -0700

    make it better.

--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ require('./config/routes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'))
-  app.use(express.static('client/public'))
+  // app.use(express.static('client/build'))
+  // app.use(express.static('client/public'))
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // )
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
-  // app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
