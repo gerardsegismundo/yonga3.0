b1aba4b514d6f130680902bcb171b2fdf5ce986f
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2021-04-09 17:33:01 -0700

    make it better

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const mongoSanitize = require('express-mongo-sanitize')
-const helmet = require('helmet')
-const cors = require('cors')
-const xss = require('xss-clean')
+// const mongoSanitize = require('express-mongo-sanitize')
+// const helmet = require('helmet')
+// const cors = require('cors')
+// const xss = require('xss-clean')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 const cloudinary = require('cloudinary')
@@ -24,10 +24,10 @@ const orderRoute = require('../routes/order.routes')
 
 // Initialization
 module.exports = app => {
-  app.use(mongoSanitize())
-  app.use(helmet())
-  app.use(cors())
-  app.use(xss())
+  // app.use(mongoSanitize())
+  // app.use(helmet())
+  // app.use(cors())
+  // app.use(xss())
   app.use(cookieParser())
   app.use(express.json({ extended: false }))
   app.use(
