19ed07687ef1303630e214aa0163f2fdf1c6df71
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2021-04-09 18:11:47 -0700

    make it better.

--- server/server.js
+++ server/server.js
@@ -15,19 +15,16 @@ require('./config/routes')(app)
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  // app.use(express.static('client/build'))
-  // app.use(express.static('client/public'))
+  app.use(express.static('client/build'))
+  app.use(express.static('client/public'))
 
-  app.get('*', (_req, res) => {
-    res.set(
-      'Content-Security-Policy',
-      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
-    )
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // )
 
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  // app.use(express.static(path.join(__dirname, 'client/build')))
 
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
 const PORT = process.env.PORT || 5000
