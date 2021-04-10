ede2d2f47d16db8634a2eb14fc037bdc2c345205
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2021-04-09 17:07:00 -0700

    make it better.

--- server/server.js
+++ server/server.js
@@ -18,11 +18,16 @@ if (process.env.NODE_ENV === 'production') {
   // app.use(express.static('client/build'))
   // app.use(express.static('client/public'))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  app.get('*', (_req, res) => {
+    res.set(
+      'Content-Security-Policy',
+      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
+    )
+
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 
   app.use(express.static(path.join(__dirname, 'client/build')))
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
 const PORT = process.env.PORT || 5000
