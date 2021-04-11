8e29fda18242ffc6060c0c84ceaa75e4104cc033
Gerard Martin R. Segismundo <gerardmartinsegismundo@gmail.com>
2021-04-11 15:16:26 -0700

    make it better

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
   app.get('*', (_req, res) => {
+    res.set(
+      'Content-Security-Policy',
+      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
+    )
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
